1ee74b4c46c6798e2000c9c54d8bf6a47e91fa14
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-04-25 11:04:11 -0700

    1

--- 11.c
+++ 11.c
@@ -1,56 +1,63 @@
-#include "main.h"
-void run_shell(void)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+void run_shell() {
+char *prompt = "simple_shell$ ";
+char *command = NULL;
+size_t command_size = 0;
+ssize_t nread_char;
+pid_t pid;
+while (1)
 {
-	char *prompt = "simple_shell$ ";
-	char *command = NULL;
-	size_t command_size = 0;
-	ssize_t nread_char;
-	pid_t pid;
-	char *args[];
+        /* Display prompt and read command from user*/
+        printf("%s", prompt);
+        nread_char = getline(&command, &command_size, stdin);
 
-	while (1)
-	{
-		/* Display prompt and read command from user*/
-		printf("%s", prompt);
-		nread_char = getline(&command, &command_size, stdin);
-		/* Handle end of file (Ctrl+D)*/
-		if (nread_char == -1)
-		{
-			printf("\n");
-			break;
-		}
-		/* Remove newline character from input*/
-		if (command[nread_char - 1] == '\n')
-		{
-			command[nread_char - 1] = '\0';
-		}
-		/* Fork a new process*/
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			/*Child process*/
-			*args[] = {command, NULL};
-			execve(command, args, NULL);
+        /* Handle end of file (Ctrl+D)*/
+        if (nread_char == -1)
+        {
+                printf("\n");
+                break;
+        }
 
-			/* If we get here, exec failed*/
-			printf("%s: command not found\n", command);
-			exit(EXIT_FAILURE);
-	}
-	else
-	{
-		/* Parent process*/
-		wait(NULL);
-	}
-	/* Free memory allocated by getline()*/
-	free(command);
+        /* Remove newline character from input*/
+        if (command[nread_char - 1] == '\n')
+        {
+                command[nread_char - 1] = '\0';
+        }
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+       /*Child process*/
+        char *args[] = {"command", NULL};
+        execve(command, args, NULL);
+
+        /* If we get here, exec failed*/
+        printf("%s: command not found\n", command);
+        exit(EXIT_FAILURE);
+    }
+     else
+    {
+        /* Parent process*/
+        wait(NULL);
+    }
 }
-int main(int argc, char *argv[])
+
+/* Free memory allocated by getline()*/
+free(command);
+
+}
+
+int main()
 {
-	run_shell();
-	return (0);
+run_shell();
+return 0;
 }
